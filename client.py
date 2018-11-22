dd36f46a43ad84b96757487e0777bbe47b142399
andrealpezr <43533560+andrealpezr@users.noreply.github.com>
2018-11-22 12:27:03 +0100

    Peticiones SIP

--- client.py
+++ client.py
@@ -1,35 +1,27 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Programa cliente que abre un socket a un servidor
-"""
 
 import socket
 import sys
 
 
 if len(sys.argv) != 3:
-    sys.exit('Usage: python3 client.py method receiver@IP=SIPport')
+    sys.exit("Usage: python client.py method receiver@IP:SIPport")
 
-METODO = sys.argv[1]
-RECEPTOR = sys.argv[2]
-IP =
-PORT =
+try:
+    Method = sys.argv[1]  # MétodoSIP
+    Receiver = sys.argv[2]  # Datos del receptor: login, IP, puerto
+    Login = Receiver.split('@')[0] + '@'  # receptor@
+    IPServer = Receiver.split('@')[1].split(':')[0]  # IP receptor
+    Port = int(Receiver.split(':')[1])  # puerto IP
+
+except (IndexError, ValueError):
+    sys.exit('Usage: python3 client.py method receiver@IP:SIPport')
 
 # Contenido que vamos a enviar
-LINE = METODO + ' ' + 'sip:' + IP + ' ' + 'SIP/2.0'
-
+Request = Method + ' ' + 'sip:' + Login + IPServer + ':' + str(Port)  + 'SIP/2.0\r\n\r\n'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  as my_socket:
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((IP, PORT))
-
-    print("Enviando: " + LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
-
-    print('Recibido -- ', data.decode('utf-8'))
-    print("Terminando socket...")
-
-print("Fin.")
+    my_socket.connect((IPServer, int(Port)))
